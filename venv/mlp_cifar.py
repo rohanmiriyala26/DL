b4662821cd3214d6955022d598f5114fee8e9896
rohanmiriyala26 <rohanmiriyala@gmail.com>
2025-07-22 14:30:29 +0530

    1st version upto evaluate

--- venv/mlp_cifar.py
+++ venv/mlp_cifar.py
@@ -14,3 +14,14 @@ y_test = to_categorical(y_test)
 #build the architecture
 model = Sequential()
 model.add(Flatten(input_shape=(32, 32, 3)))
+model.add(Dense(128, activation='relu'))
+model.add(Dense(10, activation='softmax'))
+
+#compile the model
+model.compile(optimizer='Adam', loss='categorical_crossentropy')
+
+#train the model
+model.fit(X_train, y_train, epochs=10, batch_size=64)
+
+#evaluate
+model.evaluate(X_test, y_test)
