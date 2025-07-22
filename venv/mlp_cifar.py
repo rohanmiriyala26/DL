bc8a121e5118c5f5902930bd4291c81b10ad836d
rohanmiriyala26 <rohanmiriyala@gmail.com>
2025-07-22 15:39:55 +0530

    loss plot

--- venv/mlp_cifar.py
+++ venv/mlp_cifar.py
@@ -3,6 +3,7 @@ from keras.models import Sequential
 from keras.layers import Dense,Flatten
 from keras.datasets import cifar10
 from keras.utils import to_categorical
+import matplotlib.pyplot as plt
 
 #load data
 (X_train, y_train), (X_test, y_test) = cifar10.load_data()
@@ -14,14 +15,23 @@ y_test = to_categorical(y_test)
 #build the architecture
 model = Sequential()
 model.add(Flatten(input_shape=(32, 32, 3)))
+model.add(Dense(256, activation='relu'))
 model.add(Dense(128, activation='relu'))
 model.add(Dense(10, activation='softmax'))
 
 #compile the model
-model.compile(optimizer='Adam', loss='categorical_crossentropy')
+model.compile(optimizer='Adam', loss='categorical_crossentropy', metrics=['accuracy'])
 
 #train the model
-model.fit(X_train, y_train, epochs=10, batch_size=64)
+history=model.fit(X_train, y_train, epochs=3, batch_size=64,validation_split=0.2)
+#type(history)
+#print(history)
+#print(history.history.keys())
+#pinrT(history.history.items())
 
 #evaluate
-model.evaluate(X_test, y_test)
+#accuracy,loss=model.evaluate(X_train, y_train)
+
+#visualization
+plt.plot(history.history['loss'])
+plt.show()
