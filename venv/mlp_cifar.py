29bbd4db405bf652933ee302aaefdae65a29fe76
rohanmiriyala26 <rohanmiriyala@gmail.com>
2025-07-22 16:01:57 +0530

    final version

--- venv/mlp_cifar.py
+++ venv/mlp_cifar.py
@@ -15,6 +15,8 @@ y_test = to_categorical(y_test)
 #build the architecture
 model = Sequential()
 model.add(Flatten(input_shape=(32, 32, 3)))
+model.add(Dense(1024, activation='relu'))
+model.add(Dense(512, activation='relu'))
 model.add(Dense(256, activation='relu'))
 model.add(Dense(128, activation='relu'))
 model.add(Dense(10, activation='softmax'))
@@ -23,15 +25,20 @@ model.add(Dense(10, activation='softmax'))
 model.compile(optimizer='Adam', loss='categorical_crossentropy', metrics=['accuracy'])
 
 #train the model
-history=model.fit(X_train, y_train, epochs=3, batch_size=64,validation_split=0.2)
+history=model.fit(X_train, y_train, epochs=30, batch_size=64,validation_split=0.2)
 #type(history)
 #print(history)
 #print(history.history.keys())
-#pinrT(history.history.items())
+#print(history.history.items())
 
 #evaluate
-#accuracy,loss=model.evaluate(X_train, y_train)
+test_accuracy,loss=model.evaluate(X_train, y_train)
+print(f"Test_accuracy: {test_accuracy}")
+print(f"Loss: {loss}")
 
 #visualization
-plt.plot(history.history['loss'])
+plt.plot(history.history['accuracy'],color='blue',label='train_accuracy')
+plt.plot(history.history['val_accuracy'],color='red',label='val_accuracy')
+plt.legend()
+plt.title('Epochs vs Accuracy')
 plt.show()
